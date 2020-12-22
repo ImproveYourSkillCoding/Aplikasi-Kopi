019dfdf7c46b95de8f83814ed55ec0b37c5a24dd
belajarkodecoding <belajarkodecoding@gmail.com>
2020-12-22 21:59:15 +0700

    penambahan layout selamat datang dan beberapa vector asset

---
+++ app/src/main/java/com/belajarkodecoding/aplikasicoffee/SelamatDatangActivity.java
@@ -0,0 +1,13 @@
+package com.belajarkodecoding.aplikasicoffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelamatDatangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selamat_datang);
+    }
+}
