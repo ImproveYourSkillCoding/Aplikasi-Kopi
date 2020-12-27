889326cba70cbbaf2d2f807a2ce6752ec91f0fd5
belajarkodecoding <belajarkodecoding@gmail.com>
2020-12-27 14:08:11 +0700

    sedikit pembaharuan pada menu_drawer

--- app/src/main/java/com/belajarkodecoding/aplikasicoffee/MainActivity.java
+++ app/src/main/java/com/belajarkodecoding/aplikasicoffee/MainActivity.java
@@ -59,8 +59,8 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
             case R.id.homepage:
                 fragment = new homeActivity();
                 break;
-            case R.id.riwayat:
-                fragment = new riwayatActivity();
+            case R.id.keranjang:
+                fragment = new keranjangActivity();
                 break;
             case R.id.kontak_penjual:
                 fragment = new kontakActivity();
